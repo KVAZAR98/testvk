f3727b8ea9fe2c009c230fea318697e7c9277dd6
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-20 01:58:32 +0300

    1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let b
 let a = vkBridge.send('VKWebAppGetEmail')
   .then((data) => { 
     if (data.email) {
-   b = (data.email);
+   
   
   }
   })
@@ -14,7 +14,7 @@ let a = vkBridge.send('VKWebAppGetEmail')
  
  
   console.log(a.email);
-
+console.log(a)
 
 //document.getElementById("div2").classList.remove("hidden");
 //ocument.getElementById("div1").classList.add("hidden");
