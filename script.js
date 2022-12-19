967d07b140f4571ba98e10b5a1ecf1a5a75fab27
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-19 12:16:15 +0300

    1

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+import bridge from "@vkontakte/vk-bridge";
 
-function go(){
+function go(event){
     event.preventDefault()
 document.getElementById("start").classList.add("hidden")
 document.getElementById("question").classList.remove("hidden") 
 
 }
-
-
-
-
+bridge.send('VKWebAppGetEmail')
+  .then((data) => { 
+    if (data.email) {
+      // Доступ к электронной почте получен
+    }
+  })
+  .catch((error) => {
+    // Ошибка
+    console.log(error);
+  });
 
 
 
