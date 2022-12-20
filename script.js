cd3b3b5db349b7a77d29db3ddc2724d77d6c4053
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-20 03:27:41 +0300

    1

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 
-const token = vkBridge.send('VKWebAppGetAuthToken', { 
+
+let token;
+vkBridge.send('VKWebAppGetAuthToken', { 
   app_id: 51501105, 
   scope: 'friends,status,photos'
   })
   .then((data) => { 
     if (data.access_token) {
-     alert(data.access_token)
+     token = (data.access_token)
     }
   })
  
+  vkBridge.send('VKWebAppGetUserInfo', {
+    
+    })
+    .then((data) => { 
+      if (data.id) {
+      alert(data.id)
+      }
+    })
+    .catch((error) => {
+      // Ошибка
+      console.log(error);
+    });
 //document.getElementById("div2").classList.remove("hidden");
 //ocument.getElementById("div1").classList.add("hidden");
 
