8b381547cd847f5b22c65b31494f654484d9236f
KVAZAR98 <lyljonyyy@gmail.com>
2022-12-22 02:23:47 +0300

    1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let mimg = document.createElement("img");
 let photo;
 let token;
-
+let ava123 = "fon.jpg";
 /*vkBridge.send('VKWebAppGetAuthToken', { 
   app_id: 51501105, 
   scope: 'photos'
@@ -15,22 +15,22 @@ let token;
   })*/
 vkBridge.send('VKWebAppGetUserInfo')
   .then((data) => {
-    if (data.photo_100) {
-      photo = (data.photo_100);
+    if (data.photo_200) {
+      photo = (data.photo_max_orig);
     }
   })
 
-
+  
 function go(event) {
-  
-    event.preventDefault()
-document.getElementById("start").classList.add("hidden")
-document.getElementById("question").classList.remove("hidden") 
-mimg.setAttribute('src', photo);
-document.getElementById("div3").appendChild(mimg);
+  event.preventDefault();
+
+ document.getElementById("ava").src = (photo);
+  document.getElementById("start").classList.add("hidden")
+  document.getElementById("result").classList.remove("hidden")
+
 }
-  
- 
+
+
 
 
 //document.getElementById("div2").classList.remove("hidden");
